b0b6093c03d44c83745d07891308c53039f4798a
Turro75 <valerio.turrini@gmail.com>
2021-05-14 19:38:30 +0200

    feat(gdbdiff): print less and more readable output

--- debug/gdbdiff.ts
+++ debug/gdbdiff.ts
@@ -7,13 +7,27 @@
  * Copyright (C) 2021, Uri Shaked.
  **/
 
-import { GDBClient } from '../src/utils/gdbclient';
+import { GDBClient, dumpUint32, registerNames } from '../src/utils/gdbclient';
+
+function printComparedRegisters(emulator: Uint32Array, silicone: Uint32Array) {
+  console.log('Registers \t Emulator \t     Silicone');
+  for (let i = 0; i < registerNames.length; i++) {
+    console.log(
+      registerNames[i],
+      '    \t\t0x' + dumpUint32(emulator[i]) + '\t\t 0x' + dumpUint32(silicone[i])
+    );
+  }
+}
 
 function compareRegisters(emulator: Uint32Array, silicone: Uint32Array) {
   let result = true;
   for (let i = 0; i < emulator.length; i++) {
     if (emulator[i] !== silicone[i]) {
-      console.log(`Mismatch register ${i}: emulator ${emulator[i]} != silicone ${silicone[i]}`);
+      console.log(
+        `Mismatch register ${i}: emulator 0x${emulator[i].toString(16)} != silicone 0x${silicone[
+          i
+        ].toString(16)}`
+      );
       result = false;
     }
   }
@@ -23,25 +37,34 @@ function compareRegisters(emulator: Uint32Array, silicone: Uint32Array) {
 async function main() {
   const client1 = new GDBClient();
   const client2 = new GDBClient();
-  await client1.connect('localhost');
-  await client2.connect('raspberrypi');
+  await client1.connect('localhost', 3334);
+  await client2.connect('localhost', 3333);
   // Disable interrupts
   await client1.writeRegister(19, 1, 8);
   await client2.writeRegister(19, 1, 8);
   // Start diffing
-  for (let counter = 0; ; counter++) {
+  let lastRegSet1 = await client1.readRegisters();
+  let lastRegSet2 = await client2.readRegisters();
+  let counter = 0;
+  for (counter = 0; ; counter++) {
     const regSet1 = await client1.readRegisters();
     const regSet2 = await client2.readRegisters();
-    console.log(`Instruction ${counter}`);
-    await client1.dumpRegisters();
+    //await client1.dumpRegisters();
     if (!compareRegisters(regSet1, regSet2)) {
-      console.log('PC (emulator): ', regSet1[15].toString(16));
-      console.log('PC (silicone): ', regSet2[15].toString(16));
+      console.log('status register at failed instruction:');
+      printComparedRegisters(lastRegSet1, lastRegSet2);
+      console.log('Status register at current instruction:');
+      printComparedRegisters(regSet1, regSet2);
       break;
     }
+    lastRegSet1 = regSet1;
+    lastRegSet2 = regSet2;
     await client1.singleStep();
     await client2.singleStep();
-    console.log('---');
+    if (counter % 500 === 0) {
+      console.log('Successfully compared ' + counter + ' instructions');
+    }
+    //console.log('---');
   }
 }
 
