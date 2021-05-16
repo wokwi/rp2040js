ebf5818d7100b10051163bb155b72cb8ad08db29
Turro75 <valerio.turrini@gmail.com>
2021-05-16 09:57:27 +0200

    feat(gdbdiff):automatic fix register for continuous running

--- debug/gdbdiff.ts
+++ debug/gdbdiff.ts
@@ -15,13 +15,12 @@ function printComparedRegisters(
   silicone: Uint32Array
 ) {
   for (let i = 0; i < registerNames.length; i++) {
-    let modified = '';
+    let modified = ' ';
     if (emulator[i] !== silicone[i]) {
       modified = '*';
     }
     console.log(
-      registerNames[i],
-      modified,
+      registerNames[i] + modified,
       '\t\t0x' +
         dumpUint32(registers[i]) +
         '\t0x' +
@@ -50,10 +49,15 @@ function printFlags(xpsr: number) {
   return `[${negative}${zero}${carry}${overflow}]`;
 }
 
-function compareRegisters(emulator: Uint32Array, silicone: Uint32Array) {
+async function compareFixRegisters(
+  tofixclient: GDBClient,
+  emulator: Uint32Array,
+  silicone: Uint32Array
+) {
   let result = true;
   for (let i = 0; i < emulator.length; i++) {
     if (emulator[i] !== silicone[i]) {
+      await tofixclient.writeRegister(i, silicone[i]);
       result = false;
     }
   }
@@ -69,23 +73,21 @@ async function main() {
   await client1.writeRegister(19, 1, 8);
   await client2.writeRegister(19, 1, 8);
   // Start diffing
-  let lastRegSet1 = await client1.readRegisters();
-  let lastRegSet2 = await client2.readRegisters();
+  let prevRegSet = await client2.readRegisters();
   let counter = 1;
   for (counter = 1; ; counter++) {
     const regSet1 = await client1.readRegisters();
     const regSet2 = await client2.readRegisters();
 
-    if (!compareRegisters(regSet1, regSet2)) {
-      console.log('\nMismatch after ', counter, ' compared instructions');
+    if (!(await compareFixRegisters(client1, regSet1, regSet2))) {
+      console.log('\n\nMismatch after ', counter, ' compared instructions');
       console.log('\nRegister\tStartValue\tEmulator\tSilicone');
-      printComparedRegisters(lastRegSet1, regSet1, regSet2);
-      process.exit(1);
+      printComparedRegisters(prevRegSet, regSet1, regSet2);
     }
-    lastRegSet1 = regSet1;
+    prevRegSet = regSet1;
     await client1.singleStep();
     await client2.singleStep();
-    if (counter % 500 === 0) {
+    if (counter % 50 === 0) {
       console.log(`Successfully compared ${counter} instructions`);
     }
   }
