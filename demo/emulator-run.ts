17f9f5fecbf3abefbc90a1dcf7c3c4e8524a83b8
Turro75 <valerio.turrini@gmail.com>
2021-05-22 19:33:33 +0200

    feat(gdbdiff): use the bootrom code from real pico

--- demo/emulator-run.ts
+++ demo/emulator-run.ts
@@ -1,14 +1,21 @@
 import * as fs from 'fs';
 import { RP2040 } from '../src';
-import { bootromB1 } from './bootrom';
+//import { bootromB1 } from './bootrom';
 import { loadHex } from './intelhex';
 import { GDBTCPServer } from '../src';
 
 // Create an array with the compiled code of blink
 // Execute the instructions from this array, one by one.
 const hex = fs.readFileSync('hello_uart.hex', 'utf-8');
+//bootromhex taken from pico by "dump ihex memory bootrom.hex 0x0 0x3fff"
+const bootromhex = fs.readFileSync('demo/bootrom.hex', 'utf-8');
 const mcu = new RP2040();
-mcu.loadBootrom(bootromB1);
+let localbootrom = new ArrayBuffer(0x4000);
+let localbootromview8 = new Uint8Array(localbootrom);
+let localbootromview32 = new Uint32Array(localbootrom);
+loadHex(bootromhex, localbootromview8, 0x00000000);
+mcu.loadBootrom(localbootromview32);
+//mcu.loadBootrom(bootromB1);
 loadHex(hex, mcu.flash, 0x10000000);
 
 const gdbServer = new GDBTCPServer(mcu, 3333);
@@ -19,4 +26,4 @@ mcu.uart[0].onByte = (value) => {
 };
 
 mcu.PC = 0x10000000;
-mcu.execute();
+//mcu.execute();
