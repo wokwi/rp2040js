5353e8034cbf79141304471923c29b0888aeda3c
Turro75 <valerio.turrini@gmail.com>
2021-05-23 14:00:48 +0200

    test(sio): added test -3000/2

--- src/sio.spec.ts
+++ src/sio.spec.ts
@@ -4,7 +4,7 @@ import { ICortexTestDriver } from './utils/test-driver';
 import { GDBTestDriver } from './utils/test-driver-gdb';
 import { RP2040TestDriver } from './utils/test-driver-rp2040';
 
-//HARDWARE DIVIDER REGISTERS ABSOLUTE ADDRESS
+//Hardware Divider registers absolute address
 const SIO_DIV_UDIVIDEND = SIO_START_ADDRESS + 0x060; //  Divider unsigned dividend
 const SIO_DIV_UDIVISOR = SIO_START_ADDRESS + 0x064; //  Divider unsigned divisor
 const SIO_DIV_SDIVIDEND = SIO_START_ADDRESS + 0x068; //  Divider signed dividend
@@ -44,6 +44,17 @@ describe('RPSIO', () => {
       expect(await cpu.readUint32(SIO_DIV_CSR)).toEqual(1);
     });
 
+    it('should perform a signed hardware divider -3000 / 2 = -1500 REM 0', async () => {
+      await cpu.writeUint32(SIO_DIV_SDIVIDEND, -3000);
+      expect(await cpu.readInt32(SIO_DIV_SDIVIDEND)).toEqual(-3000);
+      await cpu.writeUint32(SIO_DIV_SDIVISOR, 2);
+      expect(await cpu.readUint32(SIO_DIV_CSR)).toEqual(3);
+      expect(await cpu.readInt32(SIO_DIV_SDIVISOR)).toEqual(2);
+      expect(await cpu.readInt32(SIO_DIV_REMAINDER)).toEqual(0);
+      expect(await cpu.readInt32(SIO_DIV_QUOTIENT)).toEqual(-1500);
+      expect(await cpu.readUint32(SIO_DIV_CSR)).toEqual(1);
+    });
+
     it('should perform an unsigned hardware divider 123456 / 321 = 384 REM 192', async () => {
       await cpu.writeUint32(SIO_DIV_UDIVIDEND, 123456);
       await cpu.writeUint32(SIO_DIV_UDIVISOR, 321);
@@ -62,8 +73,8 @@ describe('RPSIO', () => {
       expect(quotient).toEqual(-384);
       await cpu.writeUint32(SIO_DIV_UDIVIDEND, 123);
       await cpu.writeUint32(SIO_DIV_UDIVISOR, 7);
-      expect(await cpu.readInt32(SIO_DIV_REMAINDER)).toEqual(4);
-      expect(await cpu.readInt32(SIO_DIV_QUOTIENT)).toEqual(17);
+      expect(await cpu.readUint32(SIO_DIV_REMAINDER)).toEqual(4);
+      expect(await cpu.readUint32(SIO_DIV_QUOTIENT)).toEqual(17);
       await cpu.writeUint32(SIO_DIV_REMAINDER, remainder);
       await cpu.writeUint32(SIO_DIV_QUOTIENT, quotient);
       expect(await cpu.readUint32(SIO_DIV_CSR)).toEqual(3);
