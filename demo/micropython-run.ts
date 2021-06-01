40cced28b736be51aeb9f82f028f59e440453463
Uri Shaked <uri@urishaked.com>
2021-06-01 20:40:29 +0300

    fix(demo): error in micropython demo

--- demo/micropython-run.ts
+++ demo/micropython-run.ts
@@ -3,14 +3,14 @@ import { RP2040 } from '../src';
 import { bootromB1 } from './bootrom';
 import { loadHex } from './intelhex';
 import { GDBTCPServer } from '../src';
-import { LogLevel } from '../src/utils/logging';
+import { ConsoleLogger, LogLevel } from '../src/utils/logging';
 
 // Create an array with the compiled code of blink
 // Execute the instructions from this array, one by one.
 const hex = fs.readFileSync('micropython.hex', 'utf-8');
 const mcu = new RP2040();
 mcu.loadBootrom(bootromB1);
-mcu.logger.currentLogLevel = LogLevel.Error;
+mcu.logger = new ConsoleLogger(LogLevel.Error);
 loadHex(hex, mcu.flash, 0x10000000);
 
 const gdbServer = new GDBTCPServer(mcu, 3333);
