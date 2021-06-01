7991d968a77ffc2508861fd63c96e5f45f63d54e
Uri Shaked <uri@urishaked.com>
2021-06-01 22:20:49 +0300

    feat(pio): tidy up the PIO assembler

--- src/utils/pio-assembler.ts
+++ src/utils/pio-assembler.ts
@@ -1,37 +1,79 @@
-export function opcodePIOJMP(Cond: number = 0, Address: number, Delay: number = 0) {
-  return ((Delay & 0x1f) << 8) | ((Cond & 0x7) << 5) | (Address & 0x1f);
+export const PIO_SRC_PINS = 0;
+export const PIO_SRC_X = 1;
+export const PIO_SRC_Y = 2;
+export const PIO_SRC_NULL = 3;
+export const PIO_SRC_STATUS = 5;
+export const PIO_SRC_ISR = 6;
+export const PIO_SRC_OSR = 7;
+
+export const PIO_DEST_PINS = 0;
+export const PIO_DEST_X = 1;
+export const PIO_DEST_Y = 2;
+export const PIO_DEST_NULL = 3;
+export const PIO_DEST_PINDIRS = 4;
+export const PIO_DEST_PC = 5;
+export const PIO_DEST_ISR = 6;
+export const PIO_DEST_EXEC = 7;
+
+export const PIO_MOV_DEST_PINS = 0;
+export const PIO_MOV_DEST_X = 1;
+export const PIO_MOV_DEST_Y = 2;
+export const PIO_MOV_DEST_EXEC = 4;
+export const PIO_MOV_DEST_PC = 5;
+export const PIO_MOV_DEST_ISR = 6;
+export const PIO_MOV_DEST_OSR = 7;
+
+export const PIO_OP_NONE = 0;
+export const PIO_OP_INVERT = 1;
+export const PIO_OP_BITREV = 2;
+
+export const PIO_WAIT_SRC_GPIO = 0;
+export const PIO_WAIT_SRC_PIN = 1;
+export const PIO_WAIT_SRC_IRQ = 2;
+
+export const PIO_COND_ALWAYS = 0;
+export const PIO_COND_NOTX = 1;
+export const PIO_COND_NOTXDEC = 2;
+export const PIO_COND_NOTY = 3;
+export const PIO_COND_NOTYDEC = 4;
+export const PIO_COND_XNEY = 5;
+export const PIO_COND_PIN = 6;
+export const PIO_COND_NOTEMPTYOSR = 7;
+
+export function pioJMP(cond: number = 0, address: number, delay: number = 0) {
+  return ((delay & 0x1f) << 8) | ((cond & 0x7) << 5) | (address & 0x1f);
 }
 
-export function opcodePIOWAIT(Pol: number, Src: number, Index: number, Delay: number = 0) {
+export function pioWAIT(pol: number, src: number, index: number, delay: number = 0) {
   return (
-    (1 << 13) | ((Delay & 0x1f) << 8) | ((Pol & 0x1) << 7) | ((Src & 0x3) << 5) | (Index & 0x1f)
+    (1 << 13) | ((delay & 0x1f) << 8) | ((pol & 0x1) << 7) | ((src & 0x3) << 5) | (index & 0x1f)
   );
 }
 
-export function opcodePIOIN(Src: number, bitCount: number, Delay: number = 0) {
-  return (2 << 13) | ((Delay & 0x1f) << 8) | ((Src & 0x7) << 5) | (bitCount & 0x1f);
+export function pioIN(src: number, bitCount: number, delay: number = 0) {
+  return (2 << 13) | ((delay & 0x1f) << 8) | ((src & 0x7) << 5) | (bitCount & 0x1f);
 }
 
-export function opcodePIOOUT(Dest: number, bitCount: number, Delay: number = 0) {
-  return (3 << 13) | ((Delay & 0x1f) << 8) | ((Dest & 0x7) << 5) | (bitCount & 0x1f);
+export function pioOUT(Dest: number, bitCount: number, delay: number = 0) {
+  return (3 << 13) | ((delay & 0x1f) << 8) | ((Dest & 0x7) << 5) | (bitCount & 0x1f);
 }
 
-export function opcodePIOPUSH(ifFull: number = 0, Blk: number, Delay: number = 0) {
-  return (4 << 13) | ((Delay & 0x1f) << 8) | ((ifFull & 1) << 6) | ((Blk & 1) << 5);
+export function pioPUSH(ifFull: number = 0, blk: number, delay: number = 0) {
+  return (4 << 13) | ((delay & 0x1f) << 8) | ((ifFull & 1) << 6) | ((blk & 1) << 5);
 }
 
-export function opcodePIOPULL(ifEmpty: number = 0, Blk: number, Delay: number = 0) {
-  return (4 << 13) | ((Delay & 0x1f) << 8) | (1 << 7) | ((ifEmpty & 1) << 6) | ((Blk & 1) << 5);
+export function pioPULL(ifEmpty: number = 0, blk: number, delay: number = 0) {
+  return (4 << 13) | ((delay & 0x1f) << 8) | (1 << 7) | ((ifEmpty & 1) << 6) | ((blk & 1) << 5);
 }
 
-export function opcodePIOMOV(Dest: number, Op: number = 0, Src: number, Delay: number = 0) {
-  return (5 << 13) | ((Delay & 0x1f) << 8) | ((Dest & 0x7) << 5) | ((Op & 0x3) << 3) | (Src & 0x3);
+export function pioMOV(dest: number, op: number = 0, src: number, delay: number = 0) {
+  return (5 << 13) | ((delay & 0x1f) << 8) | ((dest & 0x7) << 5) | ((op & 0x3) << 3) | (src & 0x3);
 }
 
-export function opcodePIOIRQ(Clr: number, Wait: number, Index: number, Delay: number = 0) {
-  return (6 << 13) | ((Delay & 0x1f) << 8) | ((Clr & 1) << 6) | ((Wait & 1) << 5) | (Index & 0x1f);
+export function pioIRQ(clr: number, wait: number, index: number, delay: number = 0) {
+  return (6 << 13) | ((delay & 0x1f) << 8) | ((clr & 1) << 6) | ((wait & 1) << 5) | (index & 0x1f);
 }
 
-export function opcodePIOSET(Dest: number, Data: number, Delay: number = 0) {
-  return (7 << 13) | ((Delay & 0x1f) << 8) | ((Dest & 0x7) << 5) | (Data & 0x1f);
+export function pioSET(dest: number, data: number, delay: number = 0) {
+  return (7 << 13) | ((delay & 0x1f) << 8) | ((dest & 0x7) << 5) | (data & 0x1f);
 }
