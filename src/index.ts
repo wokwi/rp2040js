b6f6754ca075cd606ed3e269d5c04098ae39a86d
Uri Shaked <uri@urishaked.com>
2021-07-14 00:24:12 +0300

    fix(usb): export USBCDC class from package #66
    make it easy to consume for our users

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { GDBTCPServer } from './gdbserver';
 export { GPIOPin, GPIOPinState } from './gpio-pin';
 export { RP2040 } from './rp2040';
 export { ConsoleLogger, Logger, LogLevel } from './utils/logging';
+export { USBCDC } from './usb/cdc';
