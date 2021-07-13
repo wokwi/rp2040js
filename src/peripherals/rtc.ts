5f2d2b47e74278b2d448c228540076889f5438c3
Uri Shaked <uri@urishaked.com>
2021-07-13 23:11:53 +0300

    fix(rtc): make CircuitPython happy
    implement IRQ_SETUP_0 register, which is used during CircuitPython's initialization

--- src/peripherals/rtc.ts
+++ src/peripherals/rtc.ts
@@ -1,6 +1,7 @@
 import { BasePeripheral, Peripheral } from './peripheral';
 
 const RTC_CTRL = 0x0c;
+const IRQ_SETUP_0 = 0x10;
 const RTC_ACTIVE_BITS = 0x2;
 
 export class RP2040RTC extends BasePeripheral implements Peripheral {
@@ -10,6 +11,8 @@ export class RP2040RTC extends BasePeripheral implements Peripheral {
     switch (offset) {
       case RTC_CTRL:
         return this.running ? RTC_ACTIVE_BITS : 0;
+      case IRQ_SETUP_0:
+        return 0;
     }
     return super.readUint32(offset);
   }
