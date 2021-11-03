565578777a3593cbd72a482a768bf61e8687c1a6
Martin Pittermann <martinp2250@gmail.com>
2021-11-03 12:11:48 +0100

    Fix `bitReverse` function in `pio.ts`
    pretty self-explanatory, this is another bug I found while playing around with the PIOs.
    
    https://jsfiddle.net/bwn6j75r/
    
    Cheers!

--- src/peripherals/pio.ts
+++ src/peripherals/pio.ts
@@ -81,11 +81,11 @@ export enum WaitType {
 }
 
 function bitReverse(x: number) {
-  x = ((x & 0x55555555) << 1) | ((x & 0xaaaaaaaa) >> 1);
-  x = ((x & 0x33333333) << 2) | ((x & 0xcccccccc) >> 2);
-  x = ((x & 0x0f0f0f0f) << 4) | ((x & 0xf0f0f0f0) >> 4);
-  x = ((x & 0x00ff00ff) << 8) | ((x & 0xff00ff00) >> 8);
-  x = ((x & 0x0000ffff) << 16) | ((x & 0xffff0000) >> 16);
+  x = ((x & 0x55555555) << 1) | ((x & 0xaaaaaaaa) >>> 1);
+  x = ((x & 0x33333333) << 2) | ((x & 0xcccccccc) >>> 2);
+  x = ((x & 0x0f0f0f0f) << 4) | ((x & 0xf0f0f0f0) >>> 4);
+  x = ((x & 0x00ff00ff) << 8) | ((x & 0xff00ff00) >>> 8);
+  x = ((x & 0x0000ffff) << 16) | ((x & 0xffff0000) >>> 16);
   return x >>> 0;
 }
 
