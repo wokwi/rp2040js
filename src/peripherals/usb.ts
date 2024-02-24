a0f438d906bdd55da80d4f045035000b4d94505d
Uri Shaked <uri@urishaked.com>
2024-02-24 22:47:34 +0200

    fix(usb): increase read delay
    fixes broken hello_usb test on SDK 1.5.1

--- src/peripherals/usb.ts
+++ src/peripherals/usb.ts
@@ -120,7 +120,7 @@ export class RPUSBController extends BasePeripheral {
   onEndpointWrite?: (endpoint: number, buffer: Uint8Array) => void;
   onEndpointRead?: (endpoint: number, byteCount: number) => void;
 
-  readDelayMicroseconds = 1;
+  readDelayMicroseconds = 10;
   writeDelayMicroseconds = 10; // Determined empirically
 
   get intStatus() {
